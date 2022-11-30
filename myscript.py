23886299a75e5123c3e140365491222fc3f8000f
thitesh <hiteshtekchandani1@gmail.com>
2022-11-30 12:28:34 +0530

    first relase of python tutorial

---
+++ myscript.py
@@ -0,0 +1,19 @@
+# Python program to execute
+# function directly
+def my_function():
+	print ("I am inside function")
+
+# We can test function by calling it.
+#my_function()
+
+# Python program to use
+# main for function call.
+if __name__ == "__main__":
+	my_function()
+
+import myscript
+
+#myscript.my_function()
+
+import script3
+script3.my_function1()
